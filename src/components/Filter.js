86e668e0424b7510250bb8f60f01987fa1adab4a
Daniela <danisalazarcueto7@gmail.com>
2021-10-05 11:57:59 +0100

    add proptypes to filter and add alt text to image of header

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,13 +1,24 @@
+import PropTypes from "prop-types";
 const Filter = (props) => {
   return (
     <input
       className="formulary_input"
       type="text"
       name="name"
+      placeholder="Introduce aquí el nombre de tu personaje"
       onChange={props.handleSearchBoxChange}
       value={props.searchCharacter}
     />
   );
+};
+
+Filter.defaultProps = {
+  searchCharacter: "",
+};
+
+Filter.propTypes = {
+  handleSearchBoxChange: PropTypes.func.isRequired,
+  searchCharacter: PropTypes.string,
 };
 
 export default Filter;
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,7 @@ import image from "../images/Rick_and_Morty_logo.png";
 const Header = () => {
   return (
     <header className="header">
-      <a>
-        <img className="header_img" src={image} />
-      </a>
+      <img className="header_img" src={image} alt="Rick and Morty logo" />
     </header>
   );
 };
